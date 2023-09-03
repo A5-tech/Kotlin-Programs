fc7b2eae241c9a0cc4d06b9311cd556bce9ddd04
A5-tech <anas.jamiahamdard.btech.cs@gmail.com>
2023-09-03 11:50:18 +0530

    Var and Val

---
+++ src/main/Basics/VarAndVal.kt
@@ -0,0 +1,8 @@
+fun main(){
+    var a:Int = 5;              //Declaring and Initialising var
+    val b:Int = 9;              //Declaring and Initialising val
+    println("a = $a, b = $b")   //Printing values of a and b
+    a++                         //Incrementing value of var
+//    b++                       //Incrementing value of val which will give an error
+    print("a = $a, b = $b")     //Again printing the values of a and b
+}
